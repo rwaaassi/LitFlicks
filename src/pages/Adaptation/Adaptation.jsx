2ffd185e467fc86b484dcf4e8ed1bb7c82f34d12
Rwaa Assi <rwaaassi123@gmail.com>
2024-06-30 11:29:32 +0300

    get adaptation by id

--- src/pages/Adaptation/Adaptation.jsx
+++ src/pages/Adaptation/Adaptation.jsx
@@ -1,6 +1,61 @@
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { getAdaptationById } from "../../services/adaptationsApi";
+
 const Adaptation = () => {
+  const { adaptationId } = useParams();
+  const [adaptation, setAdaptation] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchAdaptation = async () => {
+      try {
+        const adaptationData = await getAdaptationById(adaptationId);
+        setAdaptation(adaptationData);
+      } catch (error) {
+        console.error("Error fetching adaptation:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchAdaptation();
+  }, [adaptationId]);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (!adaptation) {
+    return <div>Adaptation not found</div>;
+  }
+
   return (
-    <div>Adaptation</div>
-  )
-}
-export default Adaptation
+    <div className="flex flex-col sm:flex-row gap-4">
+      <div className="flex-1 ">
+        <img
+          src={adaptation.moviePoster}
+          alt={`Poster for ${adaptation.movieTitle}`}
+          className="rounded-lg mb-4 justify-around items-center"
+          style={{ maxHeight: "300px" }}
+        />
+        <h2 className="text-xl font-bold">{adaptation.movieTitle}</h2>
+        <p className="text-gray-600">
+          Movie Duration: {adaptation.movieDuration} minutes
+        </p>
+      </div>
+      <div className="flex-1">
+        <img
+          src={adaptation.bookImage}
+          alt={`Cover for ${adaptation.bookTitle}`}
+          className="rounded-lg mb-4"
+          style={{ maxHeight: "300px" }}
+        />
+        <h3 className="text-xl font-bold">{adaptation.bookTitle}</h3>
+        <p className="text-gray-600">Book Pages: {adaptation.bookPages}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Adaptation;
--- src/pages/Adaptations/Adaptations.jsx
+++ src/pages/Adaptations/Adaptations.jsx
@@ -1,10 +1,13 @@
-import React, { useState, useEffect } from "react";
+import  { useState, useEffect } from "react";
 import { getAdaptations } from "../../services/adaptationsApi"; 
+import { useNavigate } from "react-router-dom";
+
+
 const Adaptations = () => {
   const [adaptations, setAdaptations] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
-
+  const navigate = useNavigate()
   useEffect(() => {
     const fetchAdaptations = async () => {
       try {
@@ -19,6 +22,10 @@ const Adaptations = () => {
 
     fetchAdaptations();
   }, []);
+
+   const handleAdaptationClicked = (adaptation) => {
+     navigate(`/adaptation/${adaptation.id}`);
+   };
 
   if (loading) {
     return <p>Loading...</p>;
@@ -38,7 +45,8 @@ const Adaptations = () => {
           {adaptations.map((adaptation) => (
             <li
               key={adaptation.id}
-              className="bg-white rounded-lg shadow-md p-4"
+              className="bg-white rounded-lg shadow-md p-4 cursor-pointer"
+              onClick={() => handleAdaptationClicked(adaptation)}
             >
               <div className="flex flex-col sm:flex-row gap-4">
                 <div className="flex-1 ">
