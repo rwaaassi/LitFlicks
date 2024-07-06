5aee6a7f2f2ef8a97cb4092a099e0e5375a343c4
Rwaa Assi <rwaaassi123@gmail.com>
2024-07-07 00:21:28 +0300

    styling

--- src/pages/Login/Login2.jsx
+++ src/pages/Login/Login2.jsx
@@ -2,6 +2,7 @@
 import { useState } from "react";
 import { loginUser } from "../../context/UsersApi";
 import { useNavigate } from "react-router-dom";
+import background from "../../assets/background-home-2.jpg"
 
 const LoginForm = () => {
   const [email, setEmail] = useState("");
@@ -14,15 +15,26 @@ const LoginForm = () => {
   };
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
-      <div className="max-w-md w-full space-y-8">
-        <div>
-          <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+    <div className="flex flex-col items-center justify-center min-h-screen bg-orange-50 py-12 px-4 sm:px-6 lg:px-8">
+      <div
+        className=" w-[50rem] space-y-8 h-[70vh] rounded-[15px] flex flex-col justify-center"
+        style={{
+          backgroundImage: `url(${background})`,
+          backgroundSize: "cover",
+          backgroundRepeat: "no-repeat",
+          backgroundPosition: "center",
+          backgroundColor: "rgba(0, 0, 0, 0.7)",
+          backgroundBlendMode: "overlay",
+          overflow: "hidden",
+        }}
+      >
+        <div className="text-center justify-center">
+          <h2 className="mt-6 text-center text-3xl font-extrabold text-white ">
             Sign in to your account
           </h2>
         </div>
-        <form className="mt-8 space-y-6">
-          <div className="rounded-md shadow-sm -space-y-px">
+        <form className="mt-8 space-y-6 flex flex-col items-center">
+          <div className="rounded-md shadow-sm -space-y-px flex flex-col gap-5">
             <div>
               <label htmlFor="email-address" className="sr-only">
                 Email address
@@ -33,7 +45,7 @@ const LoginForm = () => {
                 type="email"
                 autoComplete="email"
                 required
-                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                className="appearance-none rounded-[10px] relative block w-[300px]  px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                 placeholder="Email address"
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
@@ -49,7 +61,7 @@ const LoginForm = () => {
                 type="password"
                 autoComplete="current-password"
                 required
-                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                className="appearance-none rounded-[10px] relative block w-[300px] px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                 placeholder="Password"
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
@@ -61,7 +73,7 @@ const LoginForm = () => {
             <button
               type="button"
               onClick={handleLogin}
-              className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              className="group relative  flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-[#153448] hover:bg-[#2e6f9a] focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
             >
               Sign In
             </button>
--- src/pages/Login/Register.jsx
+++ src/pages/Login/Register.jsx
@@ -1,12 +1,11 @@
-// RegisterForm.js
 import { useState } from "react";
 import { registerUser } from "../../context/UsersApi";
 import { useNavigate } from "react-router-dom";
+import background from "../../assets/background-home-2.jpg"
 
 const Register = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-//   const [role, setRole] = useState("user"); 
   const navigate = useNavigate();
 
   const handleRegister = async () => {
@@ -15,15 +14,26 @@ const Register = () => {
   };
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
-      <div className="max-w-md w-full space-y-8">
-        <div>
-          <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+    <div className="flex flex-col items-center justify-center min-h-screen bg-orange-50 py-12 px-4 sm:px-6 lg:px-8">
+      <div
+        className=" w-[50rem] space-y-8 h-[70vh] rounded-[15px] flex flex-col justify-center"
+        style={{
+          backgroundImage: `url(${background})`,
+          backgroundSize: "cover",
+          backgroundRepeat: "no-repeat",
+          backgroundPosition: "center",
+          backgroundColor: "rgba(0, 0, 0, 0.7)",
+          backgroundBlendMode: "overlay",
+          overflow: "hidden",
+        }}
+      >
+        <div className="text-center justify-center">
+          <h2 className="mt-6 text-center text-3xl font-extrabold text-white">
             Create a new account
           </h2>
         </div>
-        <form className="mt-8 space-y-6">
-          <div className="rounded-md shadow-sm -space-y-px">
+        <form className="mt-8 space-y-6 flex flex-col items-center ">
+          <div className="rounded-md shadow-sm -space-y-px flex flex-col gap-5">
             <div>
               <label htmlFor="email-address" className="sr-only">
                 Email address
@@ -34,7 +44,7 @@ const Register = () => {
                 type="email"
                 autoComplete="email"
                 required
-                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                className="appearance-none rounded-[10px] w-[300px] relative block  px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                 placeholder="Email address"
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
@@ -50,7 +60,7 @@ const Register = () => {
                 type="password"
                 autoComplete="current-password"
                 required
-                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                className="appearance-none rounded-[10px] w-[300px] relative block px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                 placeholder="Password"
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
@@ -62,7 +72,7 @@ const Register = () => {
             <button
               type="button"
               onClick={handleRegister}
-              className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-[#153448] hover:bg-[#2e6f9a] focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
             >
               Register
             </button>
